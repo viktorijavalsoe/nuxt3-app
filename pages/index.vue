083bae887b8c1331959a87c92178a5085743e015
viktorija.valso <viktorija@northkingdom.com>
2021-11-29 13:28:05 +0100

    feat: add list item function

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,42 @@
 <template>
-    <h1>Home page</h1>
-    <h2>Recently viewed</h2>
-    <h2>Workspaces</h2>
+  <h1>Home page</h1>
+  <h2>Recently viewed</h2>
+  <h2>Workspaces</h2>
+  <input type="text" v-model="newWorkspaceName">
+  <button @click="createWorkspace">Create a workspace</button>
+  <ul>
+      <li v-for="workspace in workspaces" :key="workspace.id">
+          {{workspace.id}}: {{workspace.name}}
+      </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, } from 'vue';
+import { ref } from 'vue'
 
 export default defineComponent({
-    setup () {
-        
+    setup (props) {
+         const workspaces = ref([
+            {id: 1, name:'Test'},
+            {id: 2, name:'Experiment'},
+        ])  
+        const newWorkspaceName= ref('')
+        const createWorkspace = () => {
+            const randomId = Math.floor(Math.random() * 100)
+            workspaces.value.push({
+                id: randomId,
+                name: newWorkspaceName.value
+            })
+        }
+        return {
+            workspaces,
+						createWorkspace,
+						newWorkspaceName
+        }
+			}
+	})
 
-        return {}
-    }
-})
 </script>
 
 <style scoped>
