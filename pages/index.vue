bbb19e44a97f0fa589a51f4040df7ad1cd7d6773
viktorija.valso <viktorija@northkingdom.com>
2021-11-29 11:52:44 +0100

    fix: add homepage

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Home page</h1>
+    <h2>Recently viewed</h2>
+    <h2>Workspaces</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
