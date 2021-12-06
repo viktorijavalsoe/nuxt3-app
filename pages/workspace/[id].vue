a3f1ba12911f9d565184473d43f6198b2c733392
viktorija.valso <viktorija@northkingdom.com>
2021-12-06 10:49:45 +0100

    feat: add column to projects

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -1,6 +1,17 @@
 <template>
     <div>
         <h1>{{workspaceName}} Workspace (# {{ $route.params.id}})</h1>
+				<section>
+					<h2>
+						{{ board.name}}
+					</h2>
+					<button @click="createColumn">Create Column</button>
+					<div class="columnGrid">
+						<ul class="columnGrid__column" v-for="column in board.columns" :key="column.id">
+
+						</ul>
+					</div>
+				</section>
     </div>
 </template>
 
@@ -16,16 +27,39 @@ export default defineComponent({
         }
     },
 		data: ()=> ({
-				workspaceName: ''
+				workspaceName: '',
+				board: {
+					name: 'test',
+					columns: []
+					}
 		}),
+			methods: {
+				createColumn(){
+					this.board.columns.push({
+					items: []
+				})
+			}		
+		},
 		mounted(){
 			this.workspaceName = this.workspaceList.find(( workspace: Workspace) => workspace.id === Number(this.$route.params.id)).name
-			console.log(this.workspaceName)
-			console.log(this.$route.params.id)
+		
 		}
 })
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.columnGrid{
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	// grid-template-columns: repeat(var(--column-number), 1fr);
+
+	&__column{
+		border: 1px solid hotpink;
+		height: 80vh;
+		margin-right: 1rem;
+
+	}
+
+}
 
 </style>
